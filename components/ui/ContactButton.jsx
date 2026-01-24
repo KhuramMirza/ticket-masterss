a13a25826034e87f640adf1d5f6fd1b00abee815
Khuram Mirza <mirzakhuram101@gmail.com>
2026-01-25 01:10:35 +0500

    feat(components/ui): create ContactButton

---
+++ components/ui/ContactButton.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function ContactButton() {
+  console.log("clicked");
+  return (
+    <Link
+      href={`https://wa.me/${process.env.NEXT_PUBLIC_TM_PHONE_NUMBER}`}
+      className="bg-white text-accent-500 px-4 py-2 rounded-2xl font-semibold hover:scale-105 transition-all"
+    >
+      Contact Us
+    </Link>
+  );
+}
